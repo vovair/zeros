70b9cfd867974d788966c08d98c1ac0bcd36338c
vovair <vovair@gmail.com>
2018-09-18 02:15:04 +0300

    Added zeros counter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+  let result = 0;
+  for (let i = 5; number / i >= 1; i *= 5) result += Math.floor(number / i);
+  return result;
+};
